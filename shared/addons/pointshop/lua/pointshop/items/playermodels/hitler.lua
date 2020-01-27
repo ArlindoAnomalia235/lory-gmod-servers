1c3ce84948caf2f925f8622854569d79e6412fd8
ArlindoAnomalia235 <60356862+ArlindoAnomalia235@users.noreply.github.com>
2020-01-27 15:31:21 -0300

    Adicionei Adolf Hitler
    https://steamcommunity.com/sharedfiles/filedetails/?id=1381748702

---
+++ shared/addons/pointshop/lua/pointshop/items/playermodels/hitler.lua
@@ -0,0 +1,4 @@
+ITEM.Name = "Adolf Hitler"
+ITEM.Price = 20000
+ITEM.Model = "models/arachnit/wolfenstein2/adolf_hitler/adolf_hitler_ragdoll.mdl"
+ITEM.AllowedUserGroups = {"superadmin", "vip", "admin"}
